ca1f266850c560170501a89aa151f22bdd629599
Ankit Bhatt <ankitbhatt.2390@gmail.com>
2018-01-30 09:14:12 +0530

    added topics service working

--- src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -1,5 +1,8 @@
 package io.javabrains.springbootstarter.topic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class TopicController {
 	
 	@RequestMapping("/topics")
-	public String getAllTopics() {
-		return "All Topics";
+	public List<Topic> getAllTopics() {
+		List<Topic> list = new ArrayList<>();
+		list.add(new Topic("1","Java","Java Topic"));
+		list.add(new Topic("2","Javascript","Javascript Topic"));
+		return list;
 	}
 }
