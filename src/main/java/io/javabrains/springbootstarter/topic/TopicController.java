35bb2da907d66e0b99e02581efba12cad8b310ce
Ankit Bhatt <ankitbhatt.2390@gmail.com>
2018-01-31 09:36:56 +0530

    Added Topic Service with get and post implementation

--- src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -1,19 +1,33 @@
 package io.javabrains.springbootstarter.topic;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
 	
+	@Autowired
+	private TopicService topicService;
+	
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {
-		List<Topic> list = new ArrayList<>();
-		list.add(new Topic("1","Java","Java Topic"));
-		list.add(new Topic("2","Javascript","Javascript Topic"));
-		return list;
+		return topicService.getAllTopics();
 	}
+	
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="/topics")
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
+	}
+	
 }
